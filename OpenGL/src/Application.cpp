364aa17e34e5f6b15f2ac7bcb26be05214f04e79
Chaitan Divagi <natiahc@gmail.com>
2019-01-25 10:59:02 +0530

    playing more with projection matrix;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -33,7 +33,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -53,10 +53,10 @@ int main(void)
 	{
 
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f, // 0
-			 0.5f, -0.5f, 1.0f, 0.0f, // 1
-			 0.5f,  0.5f, 1.0f, 1.0f, // 2
-			-0.5f,  0.5f, 0.0f, 1.0f  // 3
+			 100.0f, 100.0f, 0.0f, 0.0f, // 0
+			 200.0f, 100.0f, 1.0f, 0.0f, // 1
+			 200.0f, 200.0f, 1.0f, 1.0f, // 2
+			 100.0f, 200.0f, 0.0f, 1.0f  // 3
 		};
 
 		unsigned int indices[] = {
@@ -81,7 +81,10 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+
+		glm::vec4 result = proj * vp;
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
